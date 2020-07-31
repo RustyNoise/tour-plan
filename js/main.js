ec7a83bca52f9613cdb509a718534b02ecffb938
RustyNoise <brylev.01@inbox.ru>
2020-07-31 14:38:06 +0700

    create: added modal window

--- js/main.js
+++ js/main.js
@@ -1,42 +1,76 @@
-var hotelSlider = new Swiper(".hotel-slider", {
-    // Optional parameters
-    loop: true,
+$(document).ready(function() {
+    var hotelSlider = new Swiper(".hotel-slider", {
+        // Optional parameters
+        loop: true,
 
-    // Navigation arrows
-    navigation: {
-        nextEl: ".hotel-slider__button--next",
-        prevEl: ".hotel-slider__button--prev",
-    },
-    speed: 450,
-    grabCursor: true,
-    autoplay: {
-        delay: 5000,
-    },
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-    },
+        // Navigation arrows
+        navigation: {
+            nextEl: ".hotel-slider__button--next",
+            prevEl: ".hotel-slider__button--prev",
+        },
+        speed: 450,
+        grabCursor: true,
+        autoplay: {
+            delay: 5000,
+        },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+    });
+    var reviewsSlider = new Swiper(".reviews-slider", {
+        // Optional parameters
+        loop: true,
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".reviews-slider__button--next",
+            prevEl: ".reviews-slider__button--prev",
+        },
+        speed: 450,
+        autoplay: {
+            delay: 6000,
+        },
+        effect: 'slide',
+    });
+
+    var menuButton = $(".menu-button");
+    menuButton.on('click', function() {
+        $(".menu-button").toggleClass('button-animation')
+        $(".navbar-bottom").toggleClass('navbar-bottom--visible');
+        $("body").toggleClass('body--noscroll');
+    });
+
+    var openModalButton = $('[data-toggle=modal]');
+    openModalButton.on('click', openModal);
+    var closemodalButton = $('[data-toggle=modal-close]');
+    closemodalButton.on('click', closeModal);
+
+    $(document).keyup(function(e) {
+        if (e.key === "Escape" || e.keyCode === 27) {
+            event.preventDefault();
+            var modalOverlay = $('.modal__overlay');
+            var modalDialog = $('.modal__dialog');
+            modalOverlay.removeClass('modal__overlay--visible');
+            modalDialog.removeClass('modal__dialog--visible');
+            $("body").removeClass('body--noscroll');
+        }
+    });
+
+    function openModal() {
+        var modalOverlay = $('.modal__overlay');
+        var modalDialog = $('.modal__dialog');
+        modalOverlay.addClass('modal__overlay--visible');
+        modalDialog.addClass('modal__dialog--visible');
+        $("body").addClass('body--noscroll');
+    }
+
+    function closeModal(event) {
+        event.preventDefault();
+        var modalOverlay = $('.modal__overlay');
+        var modalDialog = $('.modal__dialog');
+        modalOverlay.removeClass('modal__overlay--visible');
+        modalDialog.removeClass('modal__dialog--visible');
+        $("body").removeClass('body--noscroll');
+    }
 });
-var reviewsSlider = new Swiper(".reviews-slider", {
-    // Optional parameters
-    loop: true,
-
-    // Navigation arrows
-    navigation: {
-        nextEl: ".reviews-slider__button--next",
-        prevEl: ".reviews-slider__button--prev",
-    },
-    speed: 450,
-    autoplay: {
-        delay: 6000,
-    },
-    effect: 'slide',
-});
-
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function() {
-    console.log('Клик по кнопке');
-    document.querySelector(".menu-button").classList.toggle('button-animation')
-    document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible');
-    document.querySelector("body").classList.toggle('body--noscroll');
-})
